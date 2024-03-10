23dafe73b159d64f46997c5e848a1397bec296f7
Nam HQ <namhq.1989@gmail.com>
2024-03-10 09:17:04 +0700

    feat(server): format code with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -3,111 +3,111 @@ import {
   GitHubBanner,
   Refine,
   WelcomePage,
-} from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+} from '@refinedev/core'
+import { DevtoolsPanel, DevtoolsProvider } from '@refinedev/devtools'
+import { RefineKbar, RefineKbarProvider } from '@refinedev/kbar'
 
-import { useNotificationProvider } from "@refinedev/antd";
-import "@refinedev/antd/dist/reset.css";
+import { useNotificationProvider } from '@refinedev/antd'
+import '@refinedev/antd/dist/reset.css'
 
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
-} from "@refinedev/react-router-v6";
-import dataProvider from "@refinedev/simple-rest";
-import { App as AntdApp } from "antd";
-import axios from "axios";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { ColorModeContextProvider } from "./contexts/color-mode";
-import { CredentialResponse } from "./interfaces/google";
-import { parseJwt } from "./utils/parse-jwt";
+} from '@refinedev/react-router-v6'
+import dataProvider from '@refinedev/simple-rest'
+import { App as AntdApp } from 'antd'
+import axios from 'axios'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ColorModeContextProvider } from './contexts/color-mode'
+import { CredentialResponse } from './interfaces/google'
+import { parseJwt } from './utils/parse-jwt'
 
-const axiosInstance = axios.create();
+const axiosInstance = axios.create()
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem('token')
   if (config.headers) {
-    config.headers["Authorization"] = `Bearer ${token}`;
+    config.headers['Authorization'] = `Bearer ${token}`
   }
 
-  return config;
-});
+  return config
+})
 
 function App() {
   const authProvider: AuthBindings = {
     login: async ({ credential }: CredentialResponse) => {
-      const profileObj = credential ? parseJwt(credential) : null;
+      const profileObj = credential ? parseJwt(credential) : null
 
       if (profileObj) {
         localStorage.setItem(
-          "user",
+          'user',
           JSON.stringify({
             ...profileObj,
             avatar: profileObj.picture,
-          })
-        );
+          }),
+        )
 
-        localStorage.setItem("token", `${credential}`);
+        localStorage.setItem('token', `${credential}`)
 
         return {
           success: true,
-          redirectTo: "/",
-        };
+          redirectTo: '/',
+        }
       }
 
       return {
         success: false,
-      };
+      }
     },
     logout: async () => {
-      const token = localStorage.getItem("token");
+      const token = localStorage.getItem('token')
 
-      if (token && typeof window !== "undefined") {
-        localStorage.removeItem("token");
-        localStorage.removeItem("user");
-        axios.defaults.headers.common = {};
+      if (token && typeof window !== 'undefined') {
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        axios.defaults.headers.common = {}
         window.google?.accounts.id.revoke(token, () => {
-          return {};
-        });
+          return {}
+        })
       }
 
       return {
         success: true,
-        redirectTo: "/login",
-      };
+        redirectTo: '/login',
+      }
     },
     onError: async (error) => {
-      console.error(error);
-      return { error };
+      console.error(error)
+      return { error }
     },
     check: async () => {
-      const token = localStorage.getItem("token");
+      const token = localStorage.getItem('token')
 
       if (token) {
         return {
           authenticated: true,
-        };
+        }
       }
 
       return {
         authenticated: false,
         error: {
-          message: "Check failed",
-          name: "Token not found",
+          message: 'Check failed',
+          name: 'Token not found',
         },
         logout: true,
-        redirectTo: "/login",
-      };
+        redirectTo: '/login',
+      }
     },
     getPermissions: async () => null,
     getIdentity: async () => {
-      const user = localStorage.getItem("user");
+      const user = localStorage.getItem('user')
       if (user) {
-        return JSON.parse(user);
+        return JSON.parse(user)
       }
 
-      return null;
+      return null
     },
-  };
+  }
 
   return (
     <BrowserRouter>
@@ -117,7 +117,7 @@ function App() {
           <AntdApp>
             <DevtoolsProvider>
               <Refine
-                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+                dataProvider={dataProvider('https://api.fake-rest.refine.dev')}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
@@ -125,7 +125,7 @@ function App() {
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
                   useNewQueryKeys: true,
-                  projectId: "1su43K-zhrxUN-GaHjDz",
+                  projectId: '1su43K-zhrxUN-GaHjDz',
                 }}
               >
                 <Routes>
@@ -141,7 +141,7 @@ function App() {
         </ColorModeContextProvider>
       </RefineKbarProvider>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
